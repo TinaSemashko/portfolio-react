781a37110313e529b76d52c86f1e9264bd0d4b7a
TinaSemashko <akateria.satory@gmail.com>
2024-07-06 14:34:35 +0200

    font size

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -55,11 +55,11 @@ const Home: React.FC = () => {
 
   const fontSizeSubTitle = {
     xxs: '0.5rem',
-    xs: '0.5rem',
-    sm: '0.6rem',
-    md: '0.7rem',
-    lg: '0.8rem',
-    xl: '1rem',
+    xs: '0.9rem',
+    sm: '1rem',
+    md: '1.2rem',
+    lg: '1.4rem',
+    xl: '1.6rem',
     xxl: '2rem',
   };
 
